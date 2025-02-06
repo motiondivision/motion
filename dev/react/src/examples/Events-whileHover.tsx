359cfba7eb69b6b8f09e383fe64f599428618f99
Matt Perry <mattgperry@gmail.com>
2025-02-06 15:32:22 +0100

    Reverting test

--- dev/react/src/examples/Events-whileHover.tsx
+++ dev/react/src/examples/Events-whileHover.tsx
@@ -5,22 +5,19 @@ export function App() {
     const [scale, setScale] = useState(2)
     return (
         <motion.div
-            initial={{ scale: 1, transition: { duration: 0.1 } }}
             whileHover={{
-                scale: 1.5,
-                transition: { duration: 0.1 },
+                opacity: 0.5,
             }}
-            transition={{ duration: 4 }}
             onClick={() => setScale(scale + 1)}
             style={{ width: 100, height: 100, background: "white" }}
-            // transition={{
-            //     type: "spring",
-            //     mass: 1,
-            //     damping: 10,
-            //     stiffness: 60,
-            //     restDelta: 0.00001,
-            //     restSpeed: 0.00001,
-            // }}
+            transition={{
+                type: "spring",
+                mass: 1,
+                damping: 10,
+                stiffness: 60,
+                restDelta: 0.00001,
+                restSpeed: 0.00001,
+            }}
         />
     )
 }
