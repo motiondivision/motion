00181be98b6deea12ff09e80b097537bcca07a84
Matt Perry <mattgperry@gmail.com>
2025-02-06 15:30:05 +0100

    Allowing transition on initial

--- dev/react/src/examples/Events-whileHover.tsx
+++ dev/react/src/examples/Events-whileHover.tsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
 import { motion } from "framer-motion"
+import { useState } from "react"
 
 export function App() {
     const [scale, setScale] = useState(2)
     return (
         <motion.div
+            initial={{ scale: 1, transition: { duration: 0.1 } }}
             whileHover={{
-                opacity: 0.5,
+                scale: 1.5,
+                transition: { duration: 0.1 },
             }}
+            transition={{ duration: 4 }}
             onClick={() => setScale(scale + 1)}
             style={{ width: 100, height: 100, background: "white" }}
-            transition={{
-                type: "spring",
-                mass: 1,
-                damping: 10,
-                stiffness: 60,
-                restDelta: 0.00001,
-                restSpeed: 0.00001,
-            }}
+            // transition={{
+            //     type: "spring",
+            //     mass: 1,
+            //     damping: 10,
+            //     stiffness: 60,
+            //     restDelta: 0.00001,
+            //     restSpeed: 0.00001,
+            // }}
         />
     )
 }
